65e10eb9d861ce207345f8bae00ce48f68df05a4
BaeJuh <ghkslqlsl04@gmail.com>
2024-09-13 16:36:16 +0900

    routing primitive shape

--- src/index.js
+++ src/index.js
@@ -13,30 +13,57 @@ app.listen( 3000, () => {
     console.log("3000 Port is ready");
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> beebdb680910a16176a1d4846b3d0da179e47b83
-app.get( "/", (req, res) => {
-    res.render("kiosk_main_home");
+app.get("/", (req, res) => {
+    res.render("kiosk_main_home"); // touch page
 });
 
-app.get( "/home", (req, res) => {
-    res.render("kiosk_ticket_purchase");
+app.get("/home", (req, res) => {
+    res.render("kiosk_ticket_purchase"); // home
 });
+
+app.get("/movie_selection", (req, res) => {
+    res.render("Movie_Selection"); // movie choice
+});
+
+app.get( "/select_movie_time_popup", (req, res) => {
+    res.render( "select_movie_time_popup" );
+} );
+
+app.get( "/select_headcount_popup", (req, res) => {
+    console.log(req.query);
+    console.log(typeof req.query);
+    const movieData =  req.query ;
+    res.render("select_headcount_popup", {movieData: movieData});
+} );
+
+app.get( "/select_seat", (req, res) => {
+    res.render( "seat_selectionA" );
+});
+
+app.get( "/payment", (req, res) => {
+    res.render( "Payment_PointsUsage_uk" );
+} );
+
+app.get( "/earnPoint", (req, res) => {
+    res.render("payment_point_selection_popup"); // 포인트를 적립하시겠습니까?
+} );
 
 app.get( "/inputPoint", (req, res) => {
-    
-});
+    res.render("payment_Accumulation_popup"); // 포인트 적립을 위한 번호 넣기
+} );
 
 app.get( "/buying", (req, res) => {
-    
+    res.render( "payment_card_input_guide_popup" ); // 카드 입력 대기
 } );
 
 app.get( "/showPayment", (req, res) => {
-    
+    res.render("payment_summary_popup");
 } );
 
 app.get( "/printTicket", (req, res) => {
-    res.render( "ticketPrintPopup( youjung )" );
+    res.render( "ticket_print_popup" ); // 티켓 출력 하시겠습니까?
+} );
+
+app.get( "/complete", (req, res) => {
+    res.render( "payment_complete" );
 } );
