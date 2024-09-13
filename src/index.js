deb598eada110414dcf939ec2ad185feb4775cf9
ukey77 <yujin25683@gmail.com>
2024-09-13 16:10:30 +0900

    uk_routing

--- src/index.js
+++ src/index.js
@@ -4,39 +4,40 @@ import path from "path";
 
 const app = express();
 
-app.use( express.static("public") );
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 app.set("views", "view");
 
-app.listen( 3000, () => {
+app.listen(3000, () => {
     console.log("3000 Port is ready");
 });
 
-<<<<<<< HEAD
-=======
 
->>>>>>> beebdb680910a16176a1d4846b3d0da179e47b83
-app.get( "/", (req, res) => {
+app.get("/", (req, res) => {
     res.render("kiosk_main_home");
 });
 
-app.get( "/home", (req, res) => {
+app.get("/home", (req, res) => {
     res.render("kiosk_ticket_purchase");
 });
 
-app.get( "/inputPoint", (req, res) => {
-    
+app.get("/movie_selection", (req, res) => {
+    res.render("Movie_Selection");
 });
 
-app.get( "/buying", (req, res) => {
-    
-} );
+app.get("/inputPoint", (req, res) => {
 
-app.get( "/showPayment", (req, res) => {
-    
-} );
+});
 
-app.get( "/printTicket", (req, res) => {
-    res.render( "ticketPrintPopup( youjung )" );
-} );
+app.get("/buying", (req, res) => {
+
+});
+
+app.get("/showPayment", (req, res) => {
+
+});
+
+app.get("/printTicket", (req, res) => {
+    res.render("ticketPrintPopup( youjung )");
+});
